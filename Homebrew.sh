936d92c1a6eb4d4b7736447275d79e28fa8ab015
BGoodatit <81009488+BGoodatit@users.noreply.github.com>
2024-05-10 00:20:22 -0400

    Update developer.sh

--- Homebrew.sh
+++ Homebrew.sh
@@ -2,6 +2,11 @@
 # Author  : Chad Mayfield (chad@chadmayfield.com)
 # License : GPLv3
 # setup macOS using Homebrew
+# Ensure the script is running on Apple Silicon
+if [[ $(uname -m) != "arm64" ]]; then
+    echo "This script is intended only for Apple Silicon Macs."
+    exit 1
+fi
 # install rosetta on apple silicon
 if [[ "$(sysctl -n machdep.cpu.brand_string)" == *'Apple'* ]]; then
   if [ ! -d "/usr/libexec/rosetta" ]; then
@@ -38,10 +43,10 @@ if [ "$has_brew" -eq 0 ]; then
   # https://applehelpwriter.com/2018/03/21/how-homebrew-invites-users-to-get-pwned/
   # https://www.n00py.io/2016/10/privilege-escalation-on-os-x-without-exploits/
   if [[ "$(sysctl -n machdep.cpu.brand_string)" == *'Apple'* ]]; then
-    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/${USER}/.zprofile
-    echo 'eval "$(/opt/homebrew/bin/brew shelling)"' >> /Users/${USER}/.bash_profile
-    eval '"$(/opt/homebrew/bin/brew shellenv)"' | source
-    
+      # Add Homebrew to your PATH in Zsh, Bash, and Fish
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.bash_profile
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' | source
     #echo 'export PATH=/opt/homebrew/bin:$PATH' >> /Users/${USER}/.bash_profile
     #echo 'export PATH=/opt/homebrew/sbin:$PATH' >> /Users/${USER}/.bash_profile
   else
@@ -72,4 +77,47 @@ brew autoupdate status
 # display outdated apps and auto-update status
 brew cu --include-mas
 
-#EOF
+# Update Homebrew and upgrade any already-installed formulae
+brew update
+brew upgrade
+
+# Install pyenv
+brew install pyenv
+echo 'eval "$(pyenv init --path)"' >> ~/.zprofile
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+echo 'eval "$(pyenv init --path)"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'status is-login; and source (pyenv init -|psub)' >> ~/.config/fish/config.fish
+
+# Install the latest stable Python version
+LATEST_PYTHON=$(pyenv install -l | grep -v - | tail -1)
+pyenv install $LATEST_PYTHON
+pyenv global $LATEST_PYTHON
+
+# Install pip
+pyenv exec python -m ensurepip --upgrade
+
+# Install rbenv
+brew install rbenv
+echo 'eval "$(rbenv init -)"' >> ~/.zprofile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+echo 'status is-login; and rbenv init - | source' >> ~/.config/fish/config.fish
+
+# Install the latest stable Ruby version
+LATEST_RUBY=$(rbenv install -l | grep -v - | tail -1)
+rbenv install $LATEST_RUBY
+rbenv global $LATEST_RUBY
+
+# Install Node.js and npm using n
+brew install n
+n stable
+
+# Install Yarn
+brew install yarn
+
+# Refresh shell environments
+source ~/.zshrc
+source ~/.bashrc
+source ~/.config/fish/config.fish
+
+echo "Installation complete. Please restart your terminal."
